da15697e48ce6b0fd9d60bc68b3e0a739facf5e9
kemnhy <keemny@gmail.com>
2025-08-25 17:07:24 +0900

    modal 창 적용

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function (){
+  // 모달창 닫기
+  const modal = this.document.querySelector(".modal-wrap");
+  const modalClose = this.document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+  // 스크롤 이벤트
+  const topBtn = this.document.querySelector(".top-btn");
+  this.window.addEventListener("scroll", function () {
+    // console.log("스크롤시작");
+    if (this.document.documentElement.scrollTop > 200) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  });
+})
