84f812cff255176d7cc448b310ae35699de2dbd7
kemnhy <keemny@gmail.com>
2025-08-25 17:48:02 +0900

    모달창 진행 중

--- js/gotop.js
+++ js/gotop.js
@@ -1,10 +1,19 @@
-window.addEventListener("load", function (){
- // goTop버튼
+window.addEventListener("load", function () {
+  // goTop버튼
   const goTopBtn = this.document.querySelector(".gotop-btn");
   goTopBtn.addEventListener("click", function () {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+  });
+
+  this.window.addEventListener("scroll", function () {
+    // console.log("스크롤시작");
+    if (this.document.documentElement.scrollTop > 200) {
+      goTopBtn.style.display = "block";
+    } else {
+      goTopBtn.style.display = "none";
+    }
   });
 });
